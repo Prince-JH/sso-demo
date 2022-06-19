cf6c84f1156125890f92b8630e4b1abe5dfdbd62
Kim Jihoon <max.kim@ebridge-world.com>
2022-06-19 17:52:57 +0900

    feat: add requirements.txt, apply swagger

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YalliYalli Main Server",
+        default_version="v2",
+        description="YalliYalli Main Server API Swagger",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(name="test", email="test@test.com"),
+        # license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
 
+]
+
+urlpatterns += [
+    re_path(r'^sso-demo/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^sso-demo/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- user/urls_user.py
+++ user/urls_user.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import *
 # from .views_user_sign import UserSign, activate
-from .views_test import *
 from .views_user_auth import *
 from .views_user_password import *
 from .views_user_sign import *
@@ -11,7 +10,6 @@ urlpatterns = [
     # user_sign
     path('sign-in', UserSign.as_view({"post": "sign_in"}), name="sign-in"),
     path('sign-up', UserSign.as_view({"post": "sign_up"}), name="sign-up"),
-    path('test', test, name="test"),
 
     # user_auth
     path('auth', UserAuth.as_view({"post": "authenticate"}), name="authenticate"),
